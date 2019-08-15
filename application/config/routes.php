12eef154d0dd9ac40d7bccb5fe3d1dd50ed4eb3f
Muammar Zikri Aksana <muammar.clasic@gmail.com>
2019-08-15 15:10:10 +0700

    ignore theme

--- application/config/routes.php
+++ application/config/routes.php
@@ -51,6 +51,6 @@ defined('BASEPATH') or exit('No direct script access allowed');
  */
 
 // $route['default_controller'] = 'dashboard';
-$route['default_controller'] = 'welcome';
+$route['default_controller'] = 'Mza';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = false;
