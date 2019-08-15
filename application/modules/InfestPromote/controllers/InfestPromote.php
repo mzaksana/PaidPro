91e27e70d7bf2b00eafd29aaf52b5b66c083e698
Muammar Zikri Aksana <muammar.clasic@gmail.com>
2019-08-15 15:18:03 +0700

    add controller for paid promote
    2 menu and 1 func test for data base

---
+++ application/modules/InfestPromote/controllers/InfestPromote.php
@@ -0,0 +1,61 @@
+<?php
+use SleekDB\SleekDB;
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class InfestPromote extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+
+	function __construct() {
+		parent::__construct();
+		$this->_public_view= $this->config->item('public_view');
+		$this->load->helper('url');
+		// $this->load->model('Model_lib');
+	}
+	public function index()
+	{
+		/*if(!isset($_SESSION["akun"])){
+			redirect('/signIn');
+		}else{
+			$this->load->view('header',$_SESSION["data"]);
+		}*/
+		$this->load->view('index');
+	}
+
+	public function monitor()
+	{
+		$page=$this->load->view('pages/monitor');
+		return $page;
+	}
+
+	public function hashtag()
+	{
+		$page=$this->load->view('pages/hashtag');
+		return $page;
+	}
+
+	public function database()
+	{
+		require_once __DIR__."/../core/db/SleekDB.php";
+		$dataDir = __DIR__."/../core/db";
+		$newsStore = SleekDB::store('news', $dataDir);
+		// $db=new SleekDB();
+		// $db->store("temp",$dataDir);
+
+	}
+}
