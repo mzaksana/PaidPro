34be68ece6ee344cfd668003d35f005a1988d3e4
Muammar Zikri Aksana <muammar.clasic@gmail.com>
2019-08-18 05:49:16 +0700

    hashtag page

--- application/modules/InfestPromote/views/pages/hashtag.php
+++ application/modules/InfestPromote/views/pages/hashtag.php
@@ -1,79 +1,38 @@
-<div id="card"
-    class="row justify-content-center">
+<div id="card" class="row justify-content-center">
     <div class="col-lg-12">
-        <div class="btn-wrapper text-center">
+        <div id="hashtag" class="btn-wrapper text-center">
 
-
-      <div class="modal fade" id="modal-default" tabindex="-1" role="dialog" aria-labelledby="modal-default" aria-hidden="true">
-        <div class="modal-dialog modal- modal-dialog-centered modal-" role="document">
-            <div class="modal-content">
-        	
-                <div class="modal-header">
-                    <h6 class="modal-title" id="modal-title-default">Type your modal title</h6>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">×</span>
-                    </button>
-                </div>
-            
-                <div class="modal-body">
-                    
-                    <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
-                    <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
-                    
-                </div>
-                
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-primary">Save changes</button>
-                    <button type="button" class="btn btn-link  ml-auto" data-dismiss="modal">Close</button> 
-                </div>
-            </div>
-        </div>
-      </div>
-
-            <a href="#"
-                class="btn btn-neutral btn-icon list-tag" data-toggle="modal" data-target="#modal-default">
+            <a id="add" href="#"
+               class="btn btn-neutral btn-icon list-tag" data-toggle="modal" data-target="#modal-default">
                 <span class="btn-inner--text"><i class="fas fa-plus"></i></span>
             </a>
-            <a href="#"
-                class="btn btn-neutral btn-icon list-tag">
-                <span class="btn-inner--text">Google</span>
-            </a>
-            <a href="#"
-                class="btn btn-neutral btn-icon list-tag">
-                <span class="btn-inner--text">Google</span>
-            </a>
-            <a href="#"
-                class="btn btn-neutral btn-icon list-tag">
-                <span class="btn-inner--text">Google</span>
-            </a>
-            <a href="#"
-                class="btn btn-neutral btn-icon list-tag">
-                <span class="btn-inner--text">Google</span>
-            </a>
-            <a href="#"
-                class="btn btn-neutral btn-icon list-tag">
-                <span class="btn-inner--text">Google</span>
-            </a>
-            <a href="#"
-                class="btn btn-neutral btn-icon list-tag">
-                <span class="btn-inner--text">Google</span>
-            </a>
-            <a href="#"
-                class="btn btn-neutral btn-icon list-tag">
-                <span class="btn-inner--text">Google</span>
-            </a>
-            <a href="#"
-                class="btn btn-neutral btn-icon list-tag">
-                <span class="btn-inner--text">Google</span>
-            </a>
-            <a href="#"
-                class="btn btn-neutral btn-icon list-tag">
-                <span class="btn-inner--text">Google</span>
-            </a>
-            <a href="#"
-                class="btn btn-neutral btn-icon list-tag">
-                <span class="btn-inner--text">Google</span>
-            </a>
+            <?php
+            if(isset($hashtag)){
+                foreach ($hashtag as $tag){
+                    echo '<a href="#" class="btn btn-neutral btn-icon list-tag">';
+	                echo '<span class="btn-inner--text">'.$tag["name"].'</span>';
+                    echo '</a>';
+                }
+            }
+            ?>
+            <div class="modal fade" id="modal-default" tabindex="-1" role="dialog" aria-labelledby="modal-default"
+                 aria-hidden="true">
+                <div class="modal-dialog modal- modal-dialog-centered modal-" role="document">
+                    <div class="modal-content">
+                        <div class="modal-body">
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <input type="email" class="form-control" id="newHashtag" placeholder="informatics_festival_2019">
+                                </div>
+                            </div>
+                            <button type="button" class="btn btn-primary" onclick="addHashtag(this)">Add</button>
+                            <button type="button" class="btn btn-link ml-auto" data-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
 </div>
+
